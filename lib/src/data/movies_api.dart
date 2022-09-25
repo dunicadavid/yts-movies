a59915158c3b03c1d939f98972bcc7352361375f
David Dunica <46563409+dunicadavid@users.noreply.github.com>
2022-09-25 19:03:34 +0300

    upload content

---
+++ lib/src/data/movies_api.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
+import 'package:yts_movies/src/models/index.dart';
+
+class MoviesApi {
+  const MoviesApi({required String apiUrl, required Client client})
+      : _apiUrl = apiUrl,
+        _client = client;
+
+  final String _apiUrl;
+  final Client _client;
+
+  Future<List<Movie>> getMovies(int page) async {
+    String body = '';
+
+    try {
+      final Uri uri = Uri.parse('$_apiUrl/list_movies.json?page=$page');
+      final Response response = await _client.get(uri);
+
+      final String body = response.body;
+      if (response.statusCode >= 300) {
+        throw StateError(body);
+      }
+    } on SocketException catch (_) {
+      if (page > 1) {
+        return <Movie>[];
+      }
+      body = await rootBundle.loadString('res/movies.json');
+    }
+
+    final Map<String, dynamic> _body = jsonDecode(body) as Map<String, dynamic>;
+    final Map<String, dynamic> data = _body['data'] as Map<String, dynamic>;
+    final List<dynamic> movies = data['movies'] as List<dynamic>;
+
+    return movies //
+        .map((dynamic json) => Movie.fromJson(json))
+        .toList();
+  }
+}
